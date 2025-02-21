691bb1f7d0532e179244ef8685259512dbeccf19
abshek7 <abhishekallaboina@gmail.com>
2025-02-21 22:40:15 +0530

    be

--- backend/api/app.js
+++ backend/api/app.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://assignment-seven-kappa-14.vercel.app/",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
