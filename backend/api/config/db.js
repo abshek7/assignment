b32511c38f79d9b2a0c7c57788f7a74ee99d9349
abshek7 <abhishekallaboina@gmail.com>
2025-02-21 23:21:31 +0530

    be

--- backend/api/config/db.js
+++ backend/api/config/db.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 //mongoose is object relational mapping library for MongoDB and Node.js
-dotenv.config();
+dotenv.config({ path: '../.env' });
+
 const connectDB = async () => {
   try {
+    console.log("MONGO_URI:", process.env.MONGO_URI);
+
     await mongoose.connect(process.env.MONGO_URI, {
     });
     console.log('MongoDB connected');
